545f10409a132058d5a90ea7094622234efe3462
parthwal <parth21341@iiitd.ac.in>
2023-05-31 17:37:07 +0530

    mobile-proj

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import downArrow from './assets/SVG/arrow_down.svg';
 import omori from './assets/figma/omori.png';
 import undertale from './assets/figma/undertale.png';
 import punpun from './assets/figma/punpun.png';
+import ig from './assets/figma/ig.png';
+import be from './assets/figma/be.png';
+import dscrd from './assets/figma/dscrd.png';
+import at from './assets/figma/at.png';
+import spotify from './assets/figma/spotify.png';
+
+
 
 import React, { useState, useEffect, useRef } from 'react';
 
@@ -45,101 +52,127 @@ function App() {
   const arrowImage = window.innerWidth <= 768 ? outArrowRed : outArrow;
 
   return (
-    <div className="App">
-      <nav>
-        <div className="barOne">
-          <div className="name">
-            <div className="logo"></div>
-            <h1 id="parth">
-              parth
-              <br />
-              barthwal
-            </h1>
-            <ul id="nav">
-              <li>
-                <span>home</span>
-              </li>
-              <li id="projects" onClick={handleButtonClick}>
-                <span>projects</span>
+    <body>
+      <div className="App">
+        <nav>
+          <div className="barOne">
+            <div className="name">
+              <div className="logo"></div>
+              <h1 id="parth">
+                parth
+                <br />
+                barthwal
+              </h1>
+              <ul id="nav">
+                <li className="mouseHover">
+                  <span>home</span>
+                </li>
+                <li className="mouseHover" id="projects" onClick={handleButtonClick}>
+                  <span>projects</span>
+                  <img
+                    src={downArrow}
+                    alt="downArrow"
+                    className={arrowRotation ? 'rotate' : ''}
+                  />
+                  <ul
+                    ref={subNavRef}
+                    className={`subNav ${subNavVisible ? 'visible' : ''}`}
+                  >
+                    <div className="spacer"></div>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <li>
+                      <div className="projectBox"></div>
+                    </li>
+                    <div className="spacer"></div>
+                  </ul>
+                </li>
+                <li className="mouseHover">
+                  <span>parth</span>
+                </li>
+              </ul>
+            </div>
+            <div className="ctaC">
+              <button className="cta mouseHover">
+                Contact Me
                 <img
-                  src={downArrow}
+                  src={arrowImage}
                   alt="downArrow"
                   className={arrowRotation ? 'rotate' : ''}
                 />
-                <ul
-                  ref={subNavRef}
-                  className={`subNav ${subNavVisible ? 'visible' : ''}`}
-                >
-                  <div className="spacer"></div>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <li>
-                    <div className="projectBox"></div>
-                  </li>
-                  <div className="spacer"></div>
-                </ul>
-              </li>
-              <li>
-                <span>parth</span>
-              </li>
-            </ul>
+              </button>
+            </div>
           </div>
-          <div className="ctaC">
-            <button className="cta">
-              Contact Me
-              <img
-                src={arrowImage}
-                alt="downArrow"
-                className={arrowRotation ? 'rotate' : ''}
-              />
-            </button>
-          </div>
-        </div>
-      </nav>
-      <div className="contentBoxes">
-        <div className="content" id="main">
-          <div className="contentsInside">
-            <div className="badges">
-              <img src={omori} className="badge" alt="omori" />
-              <img
-                src={undertale}
-                className="badge" id="undertale"
-                alt="undertale"
-              />
-              <img src={punpun} className="badge" alt="punpun" />
+        </nav>
+        <div className="contentBoxes">
+          <div className="content" id="main">
+            <div className="contentsInside">
+              <div className="badges">
+                <img src={omori} className="badge" alt="omori" />
+                <img
+                  src={undertale}
+                  className="badge" id="undertale"
+                  alt="undertale"
+                />
+                <img src={punpun} className="badge" alt="punpun" />
+              </div>
             </div>
           </div>
         </div>
+        <footer>
+          <div className="left">
+            <div className="socials">
+              <img className='socialItem' src={ig} />
+              <img className='socialItem' src={be} />
+              <img className='socialItem' src={dscrd} />
+              <img className='socialItem' src={at} />
+            </div>
+            <div className="contact">
+              <span id="emailMe">talk to me</span>
+              <span id="email">him@pzrth.in</span>
+            </div>
+          </div>
+          <div className="right">
+            <div className="nowPlaying">
+              <img className='socialItem' src={spotify} />
+              <span>Now Playing</span>
+            </div>
+            <div className="spotifyImport">
+              <span class='spotifyData' id='songArtist'>Artist <img src={outArrowRed} /></span>
+              <span class='spotifyData' id='songName'>Name</span>
+            </div>
+          </div>
+        </footer>
       </div>
-    </div>
+    </body>
   );
 }
 
