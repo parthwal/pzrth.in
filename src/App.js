4e0b396938134dde58a16ddc0b7247ce7a62508f
parthwal <parth21341@iiitd.ac.in>
2023-05-31 09:03:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import outArrow from './assets/SVG/arrow_thickOut.svg';
+import downArrow from './assets/SVG/arrow_down.svg';
+import React, { useState } from 'react';
 
 function App() {
+  const [subNavVisible, setSubNavVisible] = useState(false);
+  const [arrowRotation, setArrowRotation] = useState(false);
+
+  const toggleSubNav = () => {
+    setSubNavVisible(!subNavVisible);
+    setArrowRotation(!arrowRotation);
+  };
+
+  const handleButtonClick = () => {
+    toggleSubNav();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="barOne">
+          <div className="name">
+            <div className="logo"></div>
+            <h1 id='parth'>parth<br />barthwal</h1>
+          <ul id="nav">
+            <li>home</li>
+            <li id="projects" onClick={handleButtonClick}>projects
+              <img
+                src={downArrow}
+                alt='downArrow'
+                className={arrowRotation ? 'rotate' : ''}
+              />
+              <ul className={`subNav ${subNavVisible ? 'visible' : ''}`}>
+                <div className='spacer'></div>
+                <li><div className='projectBox'></div></li>
+                <li><div className='projectBox'></div></li>
+                <li><div className='projectBox'></div></li>
+                <li><div className='projectBox'></div></li>
+                <li><div className='projectBox'></div></li>
+                <li><div className='projectBox'></div></li>
+                <div className='spacer'></div>
+
+              </ul>
+            </li>
+            <li>parth</li>
+          </ul>
+        </div>
+
+        <div className="cta">
+          <button className="cta">Contact Me<img src={outArrow} alt='arrow' /></button>
+        </div>
     </div>
+      </nav >
+    </div >
   );
 }
 
