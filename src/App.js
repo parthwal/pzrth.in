20eb1d0d661c1dfa0fd4b8a131f029fdd689812a
parthwal <parth21341@iiitd.ac.in>
2023-05-31 14:51:22 +0530

    mobile-proj

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import outArrow from './assets/SVG/arrow_out.svg';
 import outArrowRed from './assets/SVG/arrow_out_red.svg';
 import downArrow from './assets/SVG/arrow_down.svg';
 import React, { useState } from 'react';
-
 function App() {
   const [subNavVisible, setSubNavVisible] = useState(false);
   const [arrowRotation, setArrowRotation] = useState(false);
@@ -25,44 +24,46 @@ function App() {
           <div className="name">
             <div className="logo"></div>
             <h1 id='parth'>parth<br />barthwal</h1>
-          <ul id="nav">
-            <li>home</li>
-            <li id="projects" onClick={handleButtonClick}>projects
-              <img
-                src={downArrow}
-                alt='downArrow'
-                className={arrowRotation ? 'rotate' : ''}
-              />
-              <ul className={`subNav ${subNavVisible ? 'visible' : ''}`}>
-                <div className='spacer'></div>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <li><div className='projectBox'></div></li>
-                <div className='spacer'></div>
+            <ul id="nav">
+              <li>home</li>
+              <li id="projects" onClick={handleButtonClick}>projects
+                <img
+                  src={downArrow}
+                  alt='downArrow'
+                  className={arrowRotation ? 'rotate' : ''}
+                />
+                <ul className={`subNav ${subNavVisible ? 'visible' : ''}`}>
+                  <div className='spacer'></div>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <li><div className='projectBox'></div></li>
+                  <div className='spacer'></div>
 
-              </ul>
-            </li>
-            <li>parth</li>
-          </ul>
+                </ul>
+              </li>
+              <li>parth</li>
+            </ul>
+          </div>
+          <div className="ctaC">
+            <button className="cta">Contact Me<img
+              src={arrowImage}
+              alt='downArrow'
+              className={arrowRotation ? 'rotate' : ''}
+            /></button>
+          </div>
         </div>
-
-        <div className="cta">
-          <button className="cta">Contact Me<img
-  src={arrowImage}
-  alt='downArrow'
-  className={arrowRotation ? 'rotate' : ''}
-/></button>
-        </div>
-    </div>
       </nav >
+      <div className="contentBoxes">
+      <div className="content"></div>
+      </div>
     </div >
   );
 }
